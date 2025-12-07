6afb2b609d74b4c15bbfaa724936da4fa59ddf96
WawanHendrawan01 <wawan.hendrawan1130@gmail.com>
2025-12-07 19:55:49 +0700

    Add files via upload

---
+++ login-auth.js
@@ -0,0 +1,37 @@
+import { 
+    getAuth, 
+    signInWithEmailAndPassword 
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+
+const auth = getAuth();
+
+async function loginNow() {
+    const email = document.getElementById("email").value.trim();
+    const pass  = document.getElementById("password").value.trim();
+    const msg   = document.getElementById("msg");
+
+    msg.classList.remove("show");
+    msg.innerText = "";
+
+    try {
+        // Login Firebase Auth
+        const userCred = await signInWithEmailAndPassword(auth, email, pass);
+
+        // Simpan user ke localStorage
+        localStorage.setItem("currentUser", JSON.stringify({
+            uid: userCred.user.uid,
+            email: email,
+            role: "user" // nanti kita perbaiki role di langkah berikut
+        }));
+
+        // Redirect ke dashboard
+        window.location.href = "index.html";
+
+    } catch (err) {
+        msg.innerText = err.message;
+        msg.classList.add("show");
+    }
+}
+
+// supaya bisa dipanggil dari HTML
+window.loginNow = loginNow;
