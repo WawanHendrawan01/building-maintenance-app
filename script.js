c25c5b8217d76dc6f5b7f6a9d7c57fd3b84e6863
Wawan Hendrawan <wawan.hendrawan1130@gmail.com>
2026-08-22 16:43:36 +0700

    perbaikan jalur edit

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const db = window.firebaseDB;
-const app = window.firebaseApp;
+let db;
+let app;
 
-console.log("🔥 Firebase DB Loaded in script.js:", db);
+window.addEventListener("firebaseReady", () => {
+    db = window.firebaseDB;
+    app = window.firebaseApp;
+
+    console.log("🔥 Firebase DB Loaded in script.js:", db);
+});
 
 // --- LOGIN CHECK ---
 
