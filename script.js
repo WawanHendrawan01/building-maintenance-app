bbbb5dc331bca4828a666a84e53ec7964a4ac0f8
Wawan Hendrawan <wawan.hendrawan1130@gmail.com>
2026-08-23 17:34:14 +0700

    trtrt

--- script.js
+++ script.js
@@ -2260,6 +2260,12 @@ function renderEnergyTotals() {
     const actualBuildingCost = grossUtility - tenantRecharge;
     const hotelNetRevenue = electricity.hotelNetRevenue ?? 0;
 
+    const historyHotelNetRevenue = electricity.hotelNetRevenue ?? 0;
+
+    const historyEnergyCostRevenueRatio = historyHotelNetRevenue > 0
+    ? (actualBuildingCost / historyHotelNetRevenue) * 100
+    : 0;
+
 const energyCostRevenueRatio = hotelNetRevenue > 0
     ? (actualBuildingCost / hotelNetRevenue) * 100
     : 0;
